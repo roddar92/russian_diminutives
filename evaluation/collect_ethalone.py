99660c23524e3e3ae021cb470d1ad0120a0043ed
roddar92 <rodionowadarja@yandex.ru>
2019-07-31 00:10:31 +0300

    Test data evaluation

--- evaluation/collect_ethalone.py
+++ evaluation/collect_ethalone.py
@@ -15,13 +15,25 @@ class EthaloneCorporaCollector:
     def get_possible_dim_engings(name):
         if name.endswith('аша') or name.endswith('уля'):
             base = name[:-1] if name.endswith('аша') else name[:-3]
-            endings = ['уля', 'улечка', 'улик', 'уленька', 'утка', 'уточка']
+            endings = ['уля', 'улечка', 'улик', 'уленька', 'утка', 'уточка', 'уня', 'унечка', 'унчик']
+        elif name.endswith('ера'):
+            base = name[:-1]
+            endings = ['уля', 'улечка', 'улик', 'уленька', 'уня', 'унечка', 'унчик']
         elif name.lower().endswith('юля') or name.lower().endswith('оля') \
-                or name.endswith('уся') or name.endswith('ася'):
+                or name.endswith('уся') or name.lower().endswith('ася'):
+            base, endings = name[:-1], ['ечка', 'ик', 'енька', 'юшка', 'ютка', 'юта']
+        elif name.lower().endswith('ас'):
             base, endings = name[:-1], ['ечка', 'ик', 'енька', 'юшка']
+        elif name.lower().endswith('юта'):
+            base, endings = name[:-1], ['очка', 'ик', 'ушка']
+        elif name.lower().endswith('оня'):
+            base, endings = name[:-1], ['ечка', 'юшка']
         elif name.endswith('иша'):
             base = name[:-1]
             endings = ['ечка', 'енька', 'уля', 'улечка', 'улик', 'уленька', 'утка', 'уточка']
+        elif name.endswith('иса'):
+            base = name[:-1]
+            endings = ['очка', 'онька', 'ушка', 'уля', 'улечка', 'улик', 'уленька']
         elif name.lower().endswith('аня') or name.lower().endswith('адя') or name.endswith('атя'):
             base, endings = name[:-1], ['юша', 'юта', 'юха', 'ечка', 'енька']
         elif name.lower().endswith('рина'):
@@ -30,22 +42,38 @@ class EthaloneCorporaCollector:
         elif name.endswith('ава') or name.endswith('ара'):
             base = name[:-1]
             endings = ['очка', 'ик', 'ушка']
-        elif name.endswith('ата'):
+        elif name.endswith('ата') or name.endswith('ита'):
             base = name[:-1]
             endings = ['очка', 'ик', 'ушка', 'уля', 'улечка', 'улик', 'уленька', 'уся']
-        elif name.endswith('ина'):
+        elif name.endswith('ина') or name.lower().endswith('ида'):
             base = name[:-1]
-            endings = ['очка', 'ушка', 'уля', 'уся']
+            endings = ['очка', 'ушка', 'уша', 'уля', 'уся']
         elif name.endswith('оша'):
             base = name[:-1]
             endings = ['ечка', 'ик', 'енька']
+        elif name.endswith('еня'):
+            base = name[:-1]
+            endings = ['ечка', 'ютка', 'юта', 'юленька', 'юлечка', 'юля', 'юшка', 'юша']
+        elif name.endswith('аля'):
+            base = name[:-1]
+            endings = ['ечка', 'ик', 'енька', 'юля', 'юша']
         elif name.endswith('ова'):
             base = name[:-1]
             endings = ['очка', 'ушка']
-        elif name.endswith('ана'):
+        elif name.endswith('ана') or name.endswith('ада'):
             base = name[:-1]
-            endings = ['очка', 'ушка', 'уля']
-        elif name.endswith('ая'):
+            endings = ['очка', 'ушка', 'уля', 'уся']
+        elif name.endswith('ль') or name.endswith('уша') or name.endswith('юша'):
+            base = name[:-1]
+            endings = ['ечка', 'енька']
+        elif name.endswith('ан'):
+            base = name
+            endings = ['чик', 'уля', 'ушка']
+        elif name.endswith('ха'):
+            base = name[:-2]
+            endings = ['ша', 'шка', 'шечка', 'шенька']
+        elif name.endswith('ая') or name.endswith('ея') or name.endswith('еля') \
+                or name.endswith('ей') or name.endswith('ай'):
             base = name[:-1]
             endings = ['ечка', 'енька', 'юшка', 'юшечка', 'юшенька']
         else:
@@ -62,7 +90,6 @@ class EthaloneCorporaCollector:
                     self.ethalone[row['name']].append(base + ending)
 
         pd.DataFrame(list(self.ethalone.items()), columns=self.COL_NAMES).to_csv(path_to_output_corpus)
-
 
 
 if __name__ == '__main__':
--- evaluation/eval.py
+++ evaluation/eval.py
@@ -1,30 +1,60 @@
+import pandas as pd
+
 from evaluation.collect_ethalone import EthaloneCorporaCollector
 from rnn.diminutive_generator import DiminutiveGenerator
 from utils.dim_io import read_samples
 
 
 class DiminutiveEvaluator:
-    def __init__(self, generator, path_to_corpus):
-        assert isinstance(generator, DiminutiveGenerator)
-        self.generator = generator
-        self.ethalone_corpus = read_samples(path_to_corpus, ['name', 'dim_forms'])
+    def __init__(self, dim_generator, path_to_corpus):
+        assert isinstance(dim_generator, DiminutiveGenerator)
+        self.generator = dim_generator
+        self.ethalone_corpus = pd.read_csv(path_to_corpus, sep=',', usecols=['0', '1'])
+        self.ethalone_corpus.columns = ['name', 'dim_form']
 
     def evaluate(self, sample):
         # todo: calculate count of calls into self._find_default_transition() method! Maybe, decorator?
         correct, total = 0, 0
         for name in sample:
             dim = self.generator.generate_diminutive(name)
-            if name in self.ethalone_corpus['names'] and dim in self.ethalone_corpus['dim_forms']:
+            if name in self.ethalone_corpus['name'] and dim in self.ethalone_corpus['dim_form']:
                 correct += 1
                 total += 1
-            elif name in self.ethalone_corpus['names']:
+            elif name in self.ethalone_corpus['name']:
                 total += 1
             else:
                 base, dim_endings = EthaloneCorporaCollector.get_possible_dim_engings(name)
                 for ending in dim_endings:
                     if dim == base + ending:
                         correct += 1
-                        total += 1
                         break
+                total += 1
 
         return total, correct, correct / total
+
+
+if __name__ == '__main__':
+    CORPUS_TRAIN = '../data/train.tsv'
+    CORPUS_TEST = '../data/test.tsv'
+    CORPUS_ETHALONE = '../data/ethalone.tsv'
+
+    generator = DiminutiveGenerator(ngram=2)
+    generator.fit(CORPUS_TRAIN)
+
+    print('Evaluate generator with bigrams:')
+    evaluator = DiminutiveEvaluator(generator, CORPUS_ETHALONE)
+    train_sample = read_samples(CORPUS_TRAIN, ['name', 'dim'])
+    print('Train data (total, correct, accuracy):', evaluator.evaluate(train_sample.name))
+    test_sample = read_samples(CORPUS_TEST, ['name'])
+    print('Test data (total, correct, accuracy):', evaluator.evaluate(test_sample.name))
+    print()
+
+    generator = DiminutiveGenerator(ngram=3)
+    generator.fit(CORPUS_TRAIN)
+
+    print('Evaluate generator with trigrams:')
+    evaluator = DiminutiveEvaluator(generator, CORPUS_ETHALONE)
+    train_sample = read_samples(CORPUS_TRAIN, ['name', 'dim'])
+    print('Train data (total, correct, accuracy):', evaluator.evaluate(train_sample.name))
+    test_sample = read_samples(CORPUS_TEST, ['name'])
+    print('Test data (total, correct, accuracy):', evaluator.evaluate(test_sample.name))
