ddb3f7bac4541c5a664d83a2ef059f6bb1cfd35d
roddar92 <rodionowadarja@yandex.ru>
2019-08-19 21:53:45 +0300

    Add statistics

--- evaluation/stats_scores.py
+++ evaluation/stats_scores.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+import sys
 
 from evaluation.eval import DiminutiveEvaluator
 from rnn.diminutive_generator import DiminutiveGenerator
@@ -9,30 +9,33 @@ from utils.dim_io import read_samples
 def compute_statistics(scores):
     scores = np.array(scores)
     return {
-        "Mean: " : np.mean(train_scores, axis=0),
-        "Median: ": np.median(train_scores, axis=0),
-        "Std: ": np.std(train_scores, axis=0),
-        "Min: ": np.min(train_scores, axis=0),
-        "Max: ": np.max(train_scores, axis=0)
+        "Mean: ": np.mean(scores, axis=0),
+        "Median: ": np.median(scores, axis=0),
+        "Std: ": np.std(scores, axis=0),
+        "Min: ": np.min(scores, axis=0),
+        "Max: ": np.max(scores, axis=0)
     }
     
 
-def evaluate_stats_data(ethalone_path, train_path, train_sample, test_sample, ngram=2, times=100):
+def evaluate_stats_data(ethalone_path, train_path, train_sample, test_sample, ngram=2, times=100, fout=sys.stdout):
     gen = DiminutiveGenerator(ngram=ngram)
     gen.fit(train_path)
-    print(f'Evaluate generator with ngram={ngram}:')
+
     evaluator = DiminutiveEvaluator(gen, ethalone_path)
-    
+    print(f'Evaluate generator with ngram={ngram}:', file=fout)
+
     train_scores, test_scores = [], []
-    
-    for _ in range(times):
+    for i in range(times):
         _, _, _, accuracy, euristics = evaluator.evaluate(train_sample.name)
         train_scores.append([accuracy, euristics])
         _, _, _, accuracy, euristics = evaluator.evaluate(test_sample.name)
         test_scores.append([accuracy, euristics])
 
-    print(f'Train data (Statistics for accuracy & euristics, %): {compute_statistics(train_scores)}')
-    print(f'Test data (Statistics for accuracy & euristics, %): {compute_statistics(test_scores)}')
+        if i % 10 == 0:
+            print(f'Processed {i} times...')
+
+    print(f'Train data (Statistics for accuracy & euristics, %): {compute_statistics(train_scores)}', file=fout)
+    print(f'Test data (Statistics for accuracy & euristics, %): {compute_statistics(test_scores)}', file=fout)
 
 
 if __name__ == '__main__':
@@ -40,9 +43,10 @@ if __name__ == '__main__':
     CORPUS_TEST = '../data/test.tsv'
     CORPUS_ETHALONE = '../data/ethalone.tsv'
 
-    train_sample = read_samples(CORPUS_TRAIN, ['name', 'dim'])
-    test_sample = read_samples(CORPUS_TEST, ['name'])
+    train = read_samples(CORPUS_TRAIN, ['name', 'dim'])
+    test = read_samples(CORPUS_TEST, ['name'])
 
-    for ngram_size in (2, 3):
-        evaluate_stats_data(CORPUS_ETHALONE, CORPUS_TRAIN, train_sample, test_sample, ngram=ngram_size)
-        print()
+    with open('stats.out', 'w', encoding='utf-8') as fout:
+        for ngram_size in (2, 3):
+            evaluate_stats_data(CORPUS_ETHALONE, CORPUS_TRAIN, train, test, ngram=ngram_size, fout=fout)
+            print(file=fout)
